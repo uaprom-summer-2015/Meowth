8a624aebb43ad1d73534837f16c5493d82943eee
Kirill <saklar13@gmail.com>
2015-07-09 17:34:15 +0300

    wrong credentials fix
      - set correct user and pass in config.py
      - delete sqlite from db
      - set path db

--- config.py
+++ config.py
@@ -5,7 +5,7 @@ _basedir = os.path.abspath(os.path.dirname(__file__))
 DEBUG = True
 DEVELOPMENT = True
 SECRET_KEY = 'im!mx2m(69)b^7n3j!yi)k!a7n(^09=^&*+pnan78hl^%_yp4u'
-SQLALCHEMY_DATABASE_URI = 'postgresql://admin:admin@localhost/hrportal'
+SQLALCHEMY_DATABASE_URI = 'postgresql://root:qwerty@localhost/hrportal'
 SALT = 'useaverystrongsaltLuke'
 
 
