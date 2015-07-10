93534239529118c767c500d661fa57737c756ded
n.lyubchich <cybran111@gmail.com>
2015-07-10 16:58:05 +0300

    Logic uses real data from DB Some code style fixes

--- project/admin/logic.py
+++ project/admin/logic.py
@@ -1,13 +1,8 @@
-from collections import namedtuple
-
-# vacancy = namedtuple("vacancy", ["id", "title", "text", "category"])
-from app.feed.models import Vacancy
+from project.database import db_session
+from project.feed.models import Vacancy
 
 
 def get_vacancies():
-    # vacancies = \
-    #     vacancy(1, "python dev", "some text", "python"), \
-    #     vacancy(2, "python senior dev", "some another text", "python")
     vacancies = Vacancy.query.all()
     return vacancies
 
@@ -18,8 +13,17 @@ def get_vacancy(vacancy_id):
 
 
 def new_vacancy(data):
-    return vacancy(**data)
+
+    vacancy = Vacancy(**data)
+    db_session.add(vacancy)
+    db_session.commit()
+    return vacancy
 
 
 def update_vacancy(data):
-    return vacancy(**data)
+
+    vacancy = Vacancy.query.get(data['id'])
+    vacancy.update(**data)
+    db_session.add(vacancy)
+    db_session.commit()
+    return vacancy
--- project/database.py
+++ project/database.py
@@ -3,12 +3,14 @@ from sqlalchemy.orm import scoped_session, sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 from project import app
 
-engine = create_engine(app.config['SQLALCHEMY_DATABASE_URI'], convert_unicode=True)
+engine = create_engine(app.config['SQLALCHEMY_DATABASE_URI'],
+                       convert_unicode=True)
 db_session = scoped_session(sessionmaker(autocommit=False,
                                          autoflush=False,
                                          bind=engine))
 Base = declarative_base()
 Base.query = db_session.query_property()
+
 
 def init_db():
     import project.auth.models
