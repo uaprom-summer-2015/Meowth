0d4452322071ff5b241735fee851cf7608ec4f4c
akhalyavka <khalyavka.alexey@gmail.com>
2015-07-10 17:52:38 +0300

    IntEnum, TypeDecorator

--- project/auth/models.py
+++ project/auth/models.py
@@ -1,11 +1,27 @@
 from sqlalchemy import Column, Integer, String
 from project.database import Base, db_session
 from werkzeug.security import generate_password_hash, check_password_hash
-from project.auth.constants import TypeEnum
+from enum import IntEnum
+from sqlalchemy.types import SmallInteger, TypeDecorator
+
+
+class TypeEnum(TypeDecorator):
+
+    impl = SmallInteger
+
+    def process_bind_param(self, value, dialect):
+        return User.ROLE[value].value
+
+    def process_result_value(self, value, dialect):
+        return User.ROLE(value)
 
 
 class User(Base):
     __tablename__ = 'users'
+
+    ROLE = IntEnum('role', {'STAFF': 0,
+                            'SUPERUSER': 1})
+
     id = Column(Integer, primary_key=True)
     login = Column(String(30), unique=True)
     password = Column(String(100))
@@ -15,12 +31,12 @@ class User(Base):
     role = Column(TypeEnum())
 
     def __init__(self, login, password, email, name=None,
-                 surname=None, role='staff'):
+                 surname=None):
         self.name = name
         self.email = email
         self.login = login
         self.surname = surname
-        self.role = role
+        self.role = self.ROLE.STAFF
         self.set_password(password)
 
     def __repr__(self):
@@ -36,14 +52,15 @@ class User(Base):
         db.close()
 
     def is_superuser(self):
-        return True if self.role == 'superuser' else False
+        return self.role == self.ROLE.SUPERUSER
 
     def set_password(self, password):
         self.password = generate_password_hash(password)
 
     @staticmethod
     def create_superuser(login, password):
-        superuser = User(login, password, email=None, role='superuser')
+        superuser = User(login, password, email=None)
+        superuser.role = User.ROLE.SUPERUSER
         u = User.query.filter(User.login == login).first()
         if not u:
             superuser.save()
