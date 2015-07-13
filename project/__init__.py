9db7fdfc1360830a74ebbf54f7846c8d7c7f360b
akhalyavka <khalyavka.alexey@gmail.com>
2015-07-13 15:02:48 +0300

    little merge error

--- project/__init__.py
+++ project/__init__.py
@@ -10,5 +10,5 @@ from project.admin.views import admin_app
 from project.auth.views import auth as auth_app
 from project.feed.views import feed
 app.register_blueprint(admin_app, url_prefix='/admin')
-app.register_blueprint(AuthModule, url_prefix='/auth')
+app.register_blueprint(auth_app, url_prefix='/auth')
 app.register_blueprint(feed, url_prefix='/vacancies')
