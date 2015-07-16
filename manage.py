af8f497f9aa4dd6d3aceae75a1e58572fb292757
Olexandr <me.olexandr.kovalchuk@gmail.com>
2015-07-16 15:14:19 +0300

    Using cm in manage.py, manage.py is now executable

--- manage.py
+++ manage.py
@@ -1,8 +1,23 @@
+#!/usr/bin/env python
 from flask.ext.script import Manager
 from project import app
 from project.database import init_db as init
 from project.fixtures import load_fixtures
 import logging
+from contextlib import contextmanager
+
+
+@contextmanager
+def wrap_logging(logger, before, fail, after):
+    logger.info(before)
+    try:
+        yield
+    except Exception as e:
+        logger.error(fail)
+        logger.error(e)
+    else:
+        logger.info(after)
+
 
 manager = Manager(app)
 
@@ -10,24 +25,31 @@ manager = Manager(app)
 @manager.command
 def init_empty_db():
     """ Create empty database """
-    logging.info('Creating empty database...')
-    init()
-    logging.info('Done')
+    with wrap_logging(
+        logger=logging,
+        before='Creating empty DB ...',
+        fail='Cannot create empty DB',
+        after='Done',
+    ):
+        init()
 
 
 @manager.command
 def init_db():
     """ Create database and populate it with fixtures """
     init_empty_db()
-    logging.info('Importing fixtures...')
-    load_fixtures('init-data.json')
-    logging.info('Done')
+    with wrap_logging(
+        logger=logging,
+        before='Loading fixtures...',
+        fail='Cannot populate fixtures',
+        after='Done',
+    ):
+        load_fixtures('init-data.json')
 
 
 @manager.command
 def run():
     """ Run application """
-    logging.info('Starting app')
     app.run(debug=True)
 
 
