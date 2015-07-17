9ad0567fb0970f3e057f849cd6064f76bf3f091f
Kirill <saklar13@gmail.com>
2015-07-17 15:43:15 +0300

    models

--- project/models.py
+++ project/models.py
@@ -11,19 +11,19 @@ from project.lib.orm.types import TypeEnum
 class Vacancy(Base):
     __tablename__ = 'vacancy'
     id = Column(Integer, primary_key=True)
-    title = Column(String(100))
-    short_description = Column(String(300))
-    text = Column(Text())
+    title = Column(String(100), nullable=False)
+    short_description = Column(String(300), nullable=False)
+    text = Column(Text(), nullable=False)
     category_id = Column(Integer, ForeignKey('category.id'))
     category = relationship('Category', backref=backref('vacancies'))
-    name_in_url = Column(String(50))
-    visits = Column(Integer)
+    name_in_url = Column(String(50), nullable=False)
+    visits = Column(Integer, nullable=False, default=0)
     salary = Column(String(50))
     description = Column(String(200))  # for search spider
     keywords = Column(String(1000))
     city_id = Column(Integer, ForeignKey('city.id'))
     city = relationship('City', backref=backref('vacancies'))
-    hide = Column(Boolean)
+    hide = Column(Boolean, nullable=False, default=False)
 
     bl = Resource("bl.vacancy")
 
@@ -41,7 +41,7 @@ class Vacancy(Base):
 class Category(Base):
     __tablename__ = 'category'
     id = Column(Integer, primary_key=True)
-    name = Column(String(50))
+    name = Column(String(50), nullable=False)
 
     bl = Resource('bl.category')
 
@@ -95,7 +95,7 @@ class User(Base):
 class City(Base):
     __tablename__ = 'city'
     id = Column(Integer, primary_key=True)
-    name = Column(String(20))
+    name = Column(String(20), nullable=False)
     bl = Resource('bl.city')
 
     def __str__(self):
