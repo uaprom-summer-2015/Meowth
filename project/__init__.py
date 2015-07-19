dcc20a617e4a3012c9230959ec3e810030241dbe
Olexandr <me.olexandr.kovalchuk@gmail.com>
2015-07-19 16:06:35 +0300

    new views ad bl registered

--- project/__init__.py
+++ project/__init__.py
@@ -5,6 +5,7 @@ import logging
 import os
 from project.bl.auth import UserBL
 from project.bl.feed import CategoryBL, VacancyBL, CityBL
+from project.bl.pages import PageBL, PageBlockBL
 from project.bl.utils import registry
 
 
@@ -13,6 +14,8 @@ def init_resource_registry():
     registry['bl.vacancy'] = lambda vacancy: VacancyBL(vacancy)
     registry['bl.city'] = lambda city: CityBL(city)
     registry['bl.user'] = lambda user: UserBL(user)
+    registry['bl.pageblock'] = lambda pageblock: PageBlockBL(pageblock)
+    registry['bl.page'] = lambda page: PageBL(page)
 
 init_resource_registry()
 
@@ -28,11 +31,9 @@ db_session.rollback()
 from project.admin.views import admin_app
 from project.auth.views import auth as auth_app
 from project.feed.views import feed
-from project.pages.views import pages
 app.register_blueprint(admin_app, url_prefix='/admin')
 app.register_blueprint(auth_app, url_prefix='/auth')
 app.register_blueprint(feed, url_prefix='/vacancies')
-app.register_blueprint(pages, url_prefix='/pages')
 
 
 @app.errorhandler(413)
