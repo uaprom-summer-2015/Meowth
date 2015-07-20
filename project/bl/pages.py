7a38edc3e21087873e70e08140c34200bb02ae88
Olexandr <me.olexandr.kovalchuk@gmail.com>
2015-07-20 13:57:35 +0300

    cleaned bl for pages & pageblocks

--- project/bl/pages.py
+++ project/bl/pages.py
@@ -14,20 +14,15 @@ class PageBL(BaseBL):
         return page if page else None
 
     def save(self):
-        print(self)
+        pass  # For now
 
 
 class PageBlockBL(BaseBL):
 
-    def get_page_by_id(self, id):
-        page_model = self.model
-        page = page_model.query.get(id)
-        return page if page else None
-
-    def get_page_by_url(self, url):
-        page_model = self.model
-        page = page_model.query.filter(page_model.url == url).first()
-        return page if page else None
+    def get_block_by_id(self, id):
+        block_model = self.model
+        block = block_model.query.get(id)
+        return block if block else None
 
     def save(self):
-        print(self)
+        pass  # For now
