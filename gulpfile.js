b3291be0c46eb5e700acd45871361373384023bf
Olexandr <me.olexandr.kovalchuk@gmail.com>
2015-07-23 23:55:13 +0300

    added gulp build:scripts

--- gulpfile.js
+++ gulpfile.js
@@ -1,17 +1,37 @@
 var gulp = require('gulp');
-var config = require('./gulp.conf.json')
+var pkginfo = require('./package.json')
 
+var browserify = require('browserify')
+var buffer = require('vinyl-buffer');
 var del = require('del')
+var gutil = require('gulp-util');
+var rename = require('gulp-rename');
+var source = require('vinyl-source-stream');
+var uglify = require('gulp-uglify');
+
+
 
 gulp.task('build', ['build:scripts'])
 
+
 gulp.task('build:scripts', function() {
-  // TODO: provide code
+  return browserify({
+    entries: pkginfo.assets.scripts.entries,
+    paths: pkginfo.assets.scripts.paths
+  }).bundle()
+  .pipe(source('bundle.js'))
+  .pipe(buffer())
+  .pipe(gutil.env.type === 'production' ? uglify() : gutil.noop())
+  .pipe(gulp.dest(pkginfo.dist));
 });
 
-gulp.task('clean', function(cb) {
-  glob = config.dist + '/*';
-  return del([glob, '!.gitignore'], function(err, paths) {
-    console.log('Deleted files/folders:\n', paths.join('\n'));
-  });
+
+gulp.task('watch', ['build'], function() {
+  gulp.watch(pkginfo.assets.scripts.watches, ['build:scripts']);
+});
+
+
+gulp.task('clean', function(callback) {
+  glob = pkginfo.dist + '/*';
+  return del([glob, '!.gitignore'], callback);
 });
