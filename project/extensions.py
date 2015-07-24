27cbb1e1bf37b0dc739e5704e172b26136791d52
n.lyubchich <cybran111@gmail.com>
2015-07-24 16:47:00 +0300

    Modular extensions

--- project/extensions.py
+++ project/extensions.py
@@ -1,7 +1,9 @@
 from flask.ext.celery import Celery
+from flask.ext.sqlalchemy import SQLAlchemy
 from flask_mail import Mail
 from flask_wtf.csrf import CsrfProtect
 
 celery = Celery()
 mail = Mail()
 csrf = CsrfProtect()
+db = SQLAlchemy()
