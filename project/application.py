a632d1c7748f5f18c90d4fbfb8e88709e675d07e
n.lyubchich <cybran111@gmail.com>
2015-07-24 16:26:51 +0300

    Decoupled creation of app and extensions init

---
+++ project/application.py
@@ -0,0 +1,25 @@
+from importlib import import_module
+from flask import Flask
+from project.extensions import mail, celery, csrf
+from project.blueprints import all_blueprints
+from project.database import db
+
+
+def create_app(config_obj):
+
+    app = Flask(__name__, static_url_path='/static')
+    app.config.from_object(config_obj)
+    with app.app_context():
+        for module in app.config.get('DB_MODELS_IMPORT', list()):
+            import_module(module)
+
+    for bp in all_blueprints:
+        import_module(bp.import_name)
+        app.register_blueprint(bp)
+
+    db.init_app(app)
+    mail.init_app(app)
+    celery.init_app(app)
+    csrf.init_app(app)
+
+    return app
--- project/extensions.py
+++ project/extensions.py
@@ -1,15 +1,7 @@
+from flask.ext.celery import Celery
 from flask_mail import Mail
 from flask_wtf.csrf import CsrfProtect
-from project.celery import make_celery
-from project import app
-from project.models import PageChunk
 
-celery = make_celery(app)
-mail = Mail(app)
-CsrfProtect(app)
-
-
-@app.context_processor
-def inject_pagechunks():
-    chunks = {chunk.name: chunk.text for chunk in PageChunk.query.all()}
-    return {"pagechunks": chunks}
+celery = Celery()
+mail = Mail()
+csrf = CsrfProtect()
