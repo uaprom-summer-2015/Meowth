340165f2d7850438046d92c99b34e132740f40c2
Olexandr <me.olexandr.kovalchuk@gmail.com>
2015-07-27 14:31:25 +0300

    logging configs do not conflict with gunicorn

--- config.py
+++ config.py
@@ -43,6 +43,7 @@ class Config:
     # Logger configuration
     LOG_CONFIG = {
         'version': 1,
+        'disable_existing_loggers': False,
         'handlers': {
             'console': {
                 'class': 'logging.StreamHandler',
