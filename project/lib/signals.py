5463518fd07450da927a7f622aa1b05981acec9d
Kirill <saklar13@gmail.com>
2015-08-05 17:35:18 +0300

    pep8

--- project/lib/signals.py
+++ project/lib/signals.py
@@ -3,6 +3,7 @@ from blinker import Namespace
 my_signals = Namespace()
 model_update = my_signals.signal('model_update')
 
+
 def update(sender, **kwargs):
     from project.models import MailTemplate
     if isinstance(sender, MailTemplate):
