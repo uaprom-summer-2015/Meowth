ba1554fe71089903bfd249a9acc494558543da41
n.lyubchich <cybran111@gmail.com>
2015-08-11 14:06:47 +0300

    Added image handler for different sources

--- project/lib/media/media.py
+++ project/lib/media/media.py
@@ -1,7 +1,26 @@
-from project import app
-from flask import send_from_directory
+from cloudinary.utils import cloudinary_url
+from flask import url_for, current_app
 
 
-@app.route('/media/<path:path>')
-def get_file(path):
-    return send_from_directory('media', path)
+class ImageHandler:
+    _handler = None
+
+    def init_app(self, app):
+        self._handler = {
+            "flask": self._get_from_flask,
+            "cloudinary": self._get_from_cloudinary
+        }[app.config["STATIC_SERVER"]]
+
+    def __init__(self, app=None):
+        if app:
+            self.init_app(app)
+
+    def get_image_url(self, path):
+        return self._handler(path)
+
+    def _get_from_flask(self, path):
+        return current_app.config["SERVER_NAME"] + url_for("get_file",
+                                                           path=path)
+
+    def _get_from_cloudinary(self, path):
+        return cloudinary_url(path)
