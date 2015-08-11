e833b0019621f8c4a96e4edbdc3c641f984be857
n.lyubchich <cybran111@gmail.com>
2015-08-11 15:37:58 +0300

    Rewritten imageHandler init

--- project/extensions.py
+++ project/extensions.py
@@ -2,10 +2,11 @@ from flask.ext.celery import Celery
 from flask.ext.sqlalchemy import SQLAlchemy
 from flask_mail import Mail
 from flask_wtf.csrf import CsrfProtect
-from project.lib.media.media import ImageHandler
 
 celery = Celery()
 mail = Mail()
 csrf = CsrfProtect()
 db = SQLAlchemy()
-imageHandler = ImageHandler()
+
+from project.lib.media.media import SmartImageHandler
+imageHandler = SmartImageHandler()
