45b4c4d88ef066d76e7b3cdc0e30987eeb44d2d1
Olexandr <me.olexandr.kovalchuk@gmail.com>
2015-08-13 13:42:15 +0300

    Using pathlib

--- project/bl/uploads.py
+++ project/bl/uploads.py
@@ -1,6 +1,5 @@
 from flask import current_app
-from os import mkdir, remove
-from os.path import exists, join, splitext
+from pathlib import Path
 from project.bl.utils import BaseBL
 from project.tasks.uploads import celery_make_thumbnail
 from uuid import uuid4
@@ -12,34 +11,32 @@ class UploadedImageBL(BaseBL):
     def save_image(self, *, image, img_category, **kwargs):
 
         def mkdir_ifn_exists(dirpath):
-            if not exists(dirpath):
-                mkdir(dirpath, mode=0o751)
+            if not dirpath.exists():
+                dirpath.mkdir(mode=0o751, parents=True)
 
         if image is None:
             return
         if 'title' not in kwargs:
             kwargs['title'] = secure_filename(image.filename)
 
-        mkdir_ifn_exists(current_app.config['UPLOAD_FOLDER'])
-        category_dir = join(
+        category_dir = Path(
             current_app.config['UPLOAD_FOLDER'],
             img_category.name,
         )
-        mkdir_ifn_exists(category_dir)
 
-        thumbnail_dir = join(category_dir, 'thumb')
-        fullsized_dir = join(category_dir, 'full')
+        thumbnail_dir = category_dir / 'thumb'
+        fullsized_dir = category_dir / 'full'
         mkdir_ifn_exists(thumbnail_dir)
         mkdir_ifn_exists(fullsized_dir)
 
         uid = uuid4().hex
-        ext = splitext(image.filename)[1][1:]
+        ext = Path(image.filename).suffix[1:]
         name = "{}.{}".format(uid, ext)
-        image.save(join(fullsized_dir, name))
+        image.save(str(fullsized_dir / name))
 
         celery_make_thumbnail.delay(
-            path_to_original=join(fullsized_dir, name),
-            destination=join(thumbnail_dir, name),
+            path_to_original=str(fullsized_dir / name),
+            destination=str(thumbnail_dir / name),
             size=(75, 75),
         )
 
@@ -47,8 +44,8 @@ class UploadedImageBL(BaseBL):
             name=uid,
             ext=ext,
             img_category=img_category,
-            title=kwargs.pop('title'),
-            description=kwargs.pop('description'),
+            title=kwargs.get('title', None),
+            description=kwargs.get('description', None),
         )
         uploaded_image.bl.save()
 
@@ -56,12 +53,12 @@ class UploadedImageBL(BaseBL):
         instance = self.model
         uid = instance.name.hex
         name = "{}.{}".format(uid, instance.ext)
-        category_dir = join(
+        category_dir = Path(
             current_app.config['UPLOAD_FOLDER'],
             instance.img_category.name
         )
-        thumbnail_dir = join(category_dir, 'thumb')
-        fullsized_dir = join(category_dir, 'full')
-        remove(join(thumbnail_dir, name))
-        remove(join(fullsized_dir, name))
+        thumbnail = category_dir / 'thumb' / name
+        fullsized = category_dir / 'full' / name
+        thumbnail.unlink()
+        fullsized.unlink()
         super().delete()
