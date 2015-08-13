6b293662824c2fd9190fbe431fbf14e30f9d77d8
n.lyubchich <cybran111@gmail.com>
2015-08-13 10:28:53 +0300

    Rewritten init of SelectList for admin Page

--- project/static/src/js/page.admin.js
+++ project/static/src/js/page.admin.js
@@ -1,8 +1,40 @@
 document.addEventListener("DOMContentLoaded", (function () {
-var React = require('react');
-var PageBlockChooser = require("./admin/page");
+    var React = require('react');
+    var PageBlockList = require("./admin/select-list");
 
-React.render(React.createElement(PageBlockChooser),
-    document.getElementById("test"));
+    function parseChoices() {
+        var selects = document.getElementsByTagName("select");
+        var pageblocks = [];
 
-    }));
+        for (var i = 0; i < selects.length; i++) {
+            var e = selects[i];
+
+            pageblocks.push(
+                e.options[e.selectedIndex].value
+            );
+        }
+        return pageblocks
+    }
+
+    function parseOptions(){
+        var option_elements = document.getElementById("blocks-0").getElementsByTagName("option");
+        var options = [];
+
+        for (var i = 0; i < option_elements.length; i++) {
+            var e = option_elements[i];
+            options.push({
+                value: e.getAttribute("value"),
+                label: e.innerHTML
+            });
+        }
+        return options
+    }
+
+    React.render(React.createElement(PageBlockList, {
+            prefix: "blocks-",
+            currentChoices: parseChoices(),
+            newRowTitle: "Добавить новый блок",
+            options: parseOptions()
+        }),
+        document.getElementById("blocks"));
+}));
