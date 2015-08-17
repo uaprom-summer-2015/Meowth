f6659c48b1af7a9949fad7f481435301bbfd5b78
n.lyubchich <cybran111@gmail.com>
2015-08-17 12:05:48 +0300

    Added function for logging in tests Added csrf disabler (as a context manager)

--- project/tests/utils.py
+++ project/tests/utils.py
@@ -1,13 +1,32 @@
+from flask import url_for
 from flask.ext.testing import TestCase
 from project import create_app as app_factory
 from project.extensions import db
 from project.fixtures import load_fixtures
+from project.models import User
+
+
+class DisableCsrf:
+    def __init__(self, app):
+        self.app = app
+
+    def __enter__(self):
+        self.app.config['WTF_CSRF_ENABLED'] = False
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        self.app.config['WTF_CSRF_ENABLED'] = True
 
 
 class ProjectTestCase(TestCase):
     """
     Base class for all tests in project
     """
+    def log_in(self):
+        user = User.query.get(1)
+        # assume that login is equal to password
+        credentials = {"login": user.login, "password": user.login}
+        with DisableCsrf(self.app):
+            self.client.post(url_for("auth.login"), data=credentials)
 
     # noinspection PyAttributeOutsideInit
     def create_app(self):
