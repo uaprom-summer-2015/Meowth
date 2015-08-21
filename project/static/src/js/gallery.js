77a7f2e66fba80202aafb7da4c2f742c796c35b0
Olexandr <me.olexandr.kovalchuk@gmail.com>
2015-08-22 01:18:23 +0300

    wraped into anonymous function, using strict js

--- project/static/src/js/gallery.js
+++ project/static/src/js/gallery.js
@@ -1,6 +1,8 @@
-var $ = require('npm-zepto');
+(function ($) {
 
-$.fn.scrollToRight = function(duration) {
+  "use strict";
+
+  $.fn.scrollToRight = function(duration) {
     var $el = this;
     var el  = $el[0];
     var startPosition = el.scrollLeft
@@ -10,19 +12,19 @@ $.fn.scrollToRight = function(duration) {
     var startTime = Date.now();
 
     function scroll() {
-        var fraction = Math.min(1, (Date.now() - startTime) / dur);
-        if (isNaN(fraction)) { return; }
+      var fraction = Math.min(1, (Date.now() - startTime) / dur);
+      if (isNaN(fraction)) { return; }
 
-        el.scrollLeft = startPosition + delta * fraction;
+      el.scrollLeft = startPosition + delta * fraction;
 
-        if (fraction < 1 && $el.hasClass('playright'))  {
-            setTimeout(scroll, 15);
-        }
+      if (fraction < 1 && $el.hasClass('playright'))  {
+        setTimeout(scroll, 15);
+      }
     }
     scroll();
-};
+  };
 
-$.fn.scrollToLeft = function(duration) {
+  $.fn.scrollToLeft = function(duration) {
     var $el = this;
     var el  = $el[0];
     var startPosition = el.scrollLeft
@@ -32,53 +34,54 @@ $.fn.scrollToLeft = function(duration) {
     var startTime = Date.now();
 
     function scroll() {
-        var fraction = Math.min(1, (Date.now() - startTime) / dur);
-        if (isNaN(fraction)) { return; }
+      var fraction = Math.min(1, (Date.now() - startTime) / dur);
+      if (isNaN(fraction)) { return; }
 
-        el.scrollLeft = startPosition - delta * fraction;
+      el.scrollLeft = startPosition - delta * fraction;
 
-        if (fraction < 1 && $el.hasClass('playleft'))  {
-            setTimeout(scroll, 15);
-        }
+      if (fraction < 1 && $el.hasClass('playleft'))  {
+        setTimeout(scroll, 15);
+      }
     }
     scroll();
-};
+  };
 
 
-document.addEventListener("DOMContentLoaded", (function () {
+  document.addEventListener("DOMContentLoaded", (function () {
     require('magnific-popup');
     $('#gallery').magnificPopup({
-        delegate: 'a',
-        type: 'image',
-        closeOnContentClick: false,
-        closeBtnInside: false,
-        mainClass: 'mfp-with-zoom mfp-img-mobile',
-        image: {
-            verticalFit: true
-        },
-        gallery: {
-            enabled: true,
-            preload: [1, 1]
-        }
+      delegate: 'a',
+      type: 'image',
+      closeOnContentClick: false,
+      closeBtnInside: false,
+      mainClass: 'mfp-with-zoom mfp-img-mobile',
+      image: {
+        verticalFit: true
+      },
+      gallery: {
+        enabled: true,
+        preload: [1, 1]
+      }
     });
     $("#gallery").css("overflow-x","hidden");
     $("#gallery").css("overflow-y","hidden");
-    gal = document.getElementById('gallery');
+    var gal = document.getElementById('gallery');
     gal.addEventListener('mousemove', function(e) {
-        if (e.screenX > window.innerWidth*0.66) {
-            $('#gallery').removeClass('playleft');
-            $('#gallery').addClass('playright');
-            $('#gallery').scrollToRight(15000);
-        } else if (e.screenX < window.innerWidth*0.33) {
-            $('#gallery').removeClass('playright');
-            $('#gallery').addClass('playleft');
-            $('#gallery').scrollToLeft(15000);
-        } else {
-            $('#gallery').removeClass('playright playleft');
-        }
+      if (e.screenX > window.innerWidth*0.66) {
+        $('#gallery').removeClass('playleft');
+        $('#gallery').addClass('playright');
+        $('#gallery').scrollToRight(15000);
+      } else if (e.screenX < window.innerWidth*0.33) {
+        $('#gallery').removeClass('playright');
+        $('#gallery').addClass('playleft');
+        $('#gallery').scrollToLeft(15000);
+      } else {
+        $('#gallery').removeClass('playright playleft');
+      }
     });
     gal.addEventListener('mouseleave', function(e) {
-        $('#gallery').removeClass('playright playleft');
+      $('#gallery').removeClass('playright playleft');
     });
 
-}));
+  }));
+})(require('npm-zepto'));
